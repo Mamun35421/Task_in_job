dfbec45e4b2fd63d39fba17c6d11f2e7ffc63627
Mamun Or Rashid <86666104+Mamun35421@users.noreply.github.com>
2023-01-22 16:55:42 +0600

    done

---
+++ src/EmpListing.js
@@ -0,0 +1,64 @@
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+const EmpListing = () => {
+    const [empdata, empdatachange] = useState(null);
+    const navigate = useNavigate();
+    const LoadEdit = (id) => {
+        navigate("/employee/edit/" + id);
+    }
+    
+    useEffect(() => {
+        fetch("http://localhost:8000/candidatelisting").then((res) => {
+            return res.json();
+        }).then((resp) => {
+            empdatachange(resp);
+        }).catch((err) => {
+            console.log(err.message);
+        })
+    }, [])
+    return (
+        <div className="container">
+            <div className="card">
+                <div className="card-title">
+                    <h2>Employee Listing</h2>
+                </div>
+                <div className="card-body">
+                    <div className="divbtn">
+                        <Link to="employee/create" className="btn btn-success">Add New (+)</Link>
+                    </div>
+                    <table className="table table-bordered">
+                        <thead className="bg-dark text-white">
+                            <tr>
+                                <td>ID</td>
+                                <td>Name</td>
+                                <td>Sector</td>
+                                <td>Action</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                            {empdata &&
+                                empdata.map(item => (
+                                    <tr key={item.id}>
+                                        <td>{item.id}</td>
+                                        <td>{item.name}</td>
+                                        <td>{item.sector}</td>
+                                        <td><a onClick={() => { LoadEdit(item.id) }} className="btn btn-success">Edit</a>
+                                           
+                                           
+                                        </td>
+                                    </tr>
+                                ))
+                            }
+
+                        </tbody>
+
+                    </table>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default EmpListing;
